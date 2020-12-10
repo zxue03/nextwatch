a3d7ce8c1c1ca6096a2fbbcc8c912d7fa2a6e7b4
1 <1>
2020-12-10 15:02:39 -0500

    t

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ movieApp.use(express.json());
 movieApp.use("/api/user", authRoute);
 movieApp.use("/api/user", movieRoute);
 
-
 mongoose.connect(
         process.env.mongoConnect, 
         { useNewUrlParser: true,  useUnifiedTopology: true },
         () => console.log("Connected to db")
     );
 
-movieApp.listen(3000, () => {
+movieApp.listen(process.env.PORT, () => {
     console.log("Server is on");
 })
