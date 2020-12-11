21239bc02a9ab1b6d0e887b06f7f56a14c54db9d
tnunez01 <theornunez@gmail.com>
2020-12-11 12:35:02 -0600

    can search by person

--- public/recs.js
+++ public/recs.js
@@ -1,3 +1,7 @@
+const PAGE_MAX = 20;
+var baseImageURL, posterSize;
+var genresList = [];
+
 function getConfig() {
   let url = "https://api.themoviedb.org/3/configuration?api_key=9aadfff8aa707747cec36dc03dfe8b0f";
   fetch(url)
@@ -11,7 +15,6 @@ function getConfig() {
 
       genresList = getGenres();
       loadTrending();
-      listenForSearches();
     })
     .catch(function (err) {
       alert(err);
@@ -27,8 +30,8 @@ function getGenres() {
       genresList = data.genres;
       console.log('fetched genres:', genresList);
     })
-  
-    return genresList;
+
+  return genresList;
 }
 
 function loadTrending() {
@@ -46,6 +49,8 @@ function loadTrending() {
         toWrite += '<div class="moreInfo">';
         toWrite += '<div class="movieTitle">' + data.results[i].original_title + '</div>';
         toWrite += '<div class="synopsis">' + data.results[i].overview + '</div>';
+        //check if movie is already in their
+
         toWrite += '<input type="image" class="addButton" src="assets/add.png"';
         toWrite += ' id="' + data.results[i].id + '" value="false">';
         toWrite += '</div>';
@@ -88,42 +93,73 @@ function loadTrending() {
 
   url = "https://api.themoviedb.org/3/discover/movie?api_key=9aadfff8aa707747cec36dc03dfe8b0f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=35";
   fetch(url)
-  .then(results => results.json())
-  .then((data) => {
-    console.log(data);
-  })
+    .then(results => results.json())
+    .then((data) => {
+      console.log(data);
+    })
 }
 
-function listenForSearches() {
-  const searchButton = document.getElementById('search');
-
-  searchButton.addEventListener("click", () => {
-    searchType = document.getElementById('searchType');
-    typeSelected = searchType.selectedIndex;
-
-    if (typeSelected == mood) {
-      alert("mood");
-    } else if (typeSelected == person) {
-      searched = document.getElementById('personSearch').value;
-      searchPerson(searched);
-    }
-  });
-}
-
-/** THIS PART ISN'T WORKING...MIGHT NEED TO BE MOVED TO NODE JS FILE AND
-    CHANGE FORM ACTION TO PORT?? **/
 function searchPerson(person) {
+  var personID;
+
   let url =
-    "https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=will%20smith";
+    "https://api.themoviedb.org/3/search/person?api_key=9aadfff8aa707747cec36dc03dfe8b0f&language=en-US&query=" + person + "&page=1&include_adult=false";
   fetch(url)
     .then(results => results.json())
     .then((data) => {
-      alert("fetched?");
-      console.log("fetched person search", data);
+      personID = data.results[0].id;
+      getMovies(personID);
     })
     .catch(function (err) {
       alert(err);
     });
 }
+
+function getMovies(personID) {
+  let url = "https://api.themoviedb.org/3/discover/movie?api_key=9aadfff8aa707747cec36dc03dfe8b0f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_cast=" + personID;
+
+  fetch(url)
+    .then(results => results.json())
+    .then((data) => {
+      console.log(data.results);
+
+      var getDiv, posterURL, toWrite = "";
+      var i = 0;
+      do {
+        getDiv = 'top' + i;
+
+        if (data.results[i].poster_path == null) {
+          document.getElementById(getDiv).innerHTML = "";
+          console.log("null poster at", i);
+        } else {
+          console.log(getDiv);
+
+          posterURL = baseImageURL + posterSize + data.results[i].poster_path;
+          toWrite += '<img class="moviePoster"';
+          toWrite += 'src="' + posterURL + '">';
+
+          toWrite += '<div class="moreInfo">';
+          toWrite += '<div class="movieTitle">' + data.results[i].original_title + '</div>';
+          toWrite += '<div class="synopsis">' + data.results[i].overview + '</div>';
+
+          toWrite += '<input type="image" class="addButton" src="assets/add.png"';
+          toWrite += ' id="' + data.results[i].id + '" value="false">';
+          toWrite += '</div>';
+
+          document.getElementById(getDiv).innerHTML = toWrite;
+          toWrite = "";
+        }
+        i++;
+      } while (i < data.results.length);
+    })
+    .catch(function (err) {
+      alert(err);
+    });
+}
+
+function loadMovies(data) {
+
+}
+
 
 document.addEventListener('DOMContentLoaded', getConfig);
